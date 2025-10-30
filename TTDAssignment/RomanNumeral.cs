cd06173edb030dec3cbcff05022222a6a232de83
Hellfire24 <codenamed_hellfire@hotmail.com>
2025-10-30 01:37:22 +0100

    Add ValidateInput for minimal passing

--- TTDAssignment/RomanNumeral.cs
+++ TTDAssignment/RomanNumeral.cs
@@ -1,8 +1,13 @@
-class RomanNumeral
+public class RomanNumeral
 {
   private string input;
+
   public RomanNumeral(string input)
   {
     this.input = input;
-  }  
+  }
+  public bool ValidateInput()
+  {
+    return false;
+  }
 }
