ccf2820cf531315703b4cbb318a8d275a726a826
Hellfire24 <codenamed_hellfire@hotmail.com>
2025-10-30 01:35:09 +0100

    Add Test for input is not convertable to int

--- TTDAssignment.Tests/UnitTest1.cs
+++ TTDAssignment.Tests/UnitTest1.cs
@@ -7,4 +7,13 @@ public class UnitTest1
     {
       Assert.True(true);
     }
+
+    [Fact]
+    public void Test_Input_Is_String()
+    {
+      var romanNumeral = new RomanNumeral("TEST");
+      Assert.False(romanNumeral.ValidateInput());
+      Assert.IsNotType<int>(romanNumeral.ValidateInput());
+    }
+
 }
